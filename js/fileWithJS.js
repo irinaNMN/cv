420ec15276a8b7e4cebc601ee2919eb43faafbd2
Irina Plesca <irina.cretu2112@gmail.com>
2024-06-28 15:38:47 +0300

    My project is ready!

--- js/fileWithJS.js
+++ js/fileWithJS.js
@@ -4,7 +4,34 @@ function showTime() {
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
-    clockElement.textContent= `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
 setInterval(showTime, 1000);
-showTime();
+window.onload = function () {
+    showTime();
+    document.getElementById('likeCount').innerHTML = localStorage.getItem('likeCount');
+    document.getElementById('dislikeCount').innerHTML = localStorage.getItem('dislikeCount');
+}
+
+function addLike() {
+    let likeCount = localStorage.getItem('likeCount');
+    if (likeCount) {
+        likeCount = Number(likeCount) + 1;
+    } else {
+        likeCount = 1;
+    }
+    localStorage.setItem('likeCount', likeCount);
+    document.getElementById('likeCount').innerHTML = likeCount;
+}
+
+function addDisLike() {
+    let dislikeCount = localStorage.getItem('dislikeCount');
+    if (dislikeCount) {
+        dislikeCount = Number(dislikeCount) + 1;
+    } else {
+        dislikeCount = 1;
+    }
+    localStorage.setItem('dislikeCount', dislikeCount);
+    document.getElementById('dislikeCount').innerHTML = dislikeCount;
+}
